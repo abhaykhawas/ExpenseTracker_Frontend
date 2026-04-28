ffa3dd21b6ec1c121b8e15c11dee203b1af38bd3
Abhay Khawas <abhaykhawas@gmail.com>
2026-04-28 22:18:02 +0530

    Changed url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://expensetracker-backend-khjs.onrender.com/api'
 })
 
 API.interceptors.request.use((req) => {
